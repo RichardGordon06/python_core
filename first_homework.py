c9e462bad47e7fb1dd67bfafdbbe7e2548c44d68
Pavel Vlasov <richardgordon06@gmail.com>
2026-09-15 18:52:27 +0300

    Initial commit

---
+++ first_homework.py
@@ -0,0 +1,16 @@
+number_1 = round(-1.66)
+
+print(int(number_1)),
+print((int(-1.66)), '\n')
+
+web_site = 'www.my_site.com#about'
+print((web_site.replace('#', '/')), '\n')
+
+print('stroka' + 'ing', '\n')
+
+name = 'Ivanou Ivan'
+stroka = name.split()
+print(stroka[1], stroka[0], '\n')
+
+stroka_probel = input("\nВведине вашу строку: ")
+print(stroka_probel.strip(),'\n')
