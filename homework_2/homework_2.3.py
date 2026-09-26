9a0a7d257086dd02f0390c2cebb1bde8ea34566a
Pavel Vlasov <richardgordon06@gmail.com>
2026-09-26 23:55:32 +0300

    bug_fix homework_2

--- homework_2/homework_2.3.py
+++ homework_2/homework_2.3.py
@@ -1,7 +1,7 @@
 for i in range(1, 21):
     if i % 5 == 0:
         print('Тест пропущен')
-        pass
+        continue
     elif i == 18:
         print('Завершение тестирования')
         break
